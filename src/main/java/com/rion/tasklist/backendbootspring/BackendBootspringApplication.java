656cfdf19ead3eb538c97091ee968e99974c4411
rion <riongm86@gmail.com>
2022-08-02 19:14:50 +0300

    rirst

---
+++ src/main/java/com/rion/tasklist/backendbootspring/BackendBootspringApplication.java
@@ -0,0 +1,13 @@
+package com.rion.tasklist.backendbootspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBootspringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendBootspringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rion/tasklist/backendbootspring/BackendBootspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rion.tasklist.backendbootspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendBootspringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
